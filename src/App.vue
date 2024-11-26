d6699ca5962f3f69df01d644be7c900a56b8f990
andregustavoxs <andrewinstitucional@gmail.com>
2024-11-26 11:07:17 -0300

    #22 - Finalizando cabeçalho e rodapé do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <Navbar></Navbar>
+  <Navbar :logo="logo_src" :alt="app_name"></Navbar>
   <router-view/>
   <Footer />
 </template>
 
 <style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
 <script>
 import Navbar from "@/components/Navbar.vue";
 import Footer from "@/components/Footer.vue"
 
-
 export default {
   components: {
     Navbar,
     Footer,
+  },
+  data() {
+    return {
+      logo_src: "img/logo.png",
+      app_name: "Make your Burger",
+    }
   }
 }
 </script>
