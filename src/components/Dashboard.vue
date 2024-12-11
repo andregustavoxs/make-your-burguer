3c20c75439af94d08919904ec2882d125220e667
andregustavoxs <andrewinstitucional@gmail.com>
2024-12-11 11:47:56 -0300

    #30 - Recebendo os status dos pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -24,6 +24,7 @@
         <div>
           <select name="status" class="status">
             <option value="">Selecione</option>
+            <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status === s.tipo">{{ s.tipo }}</option>
           </select>
           <button class="delete-btn">Cancelar</button>
         </div>
@@ -50,7 +51,14 @@ export default {
 
       this.burgers = data;
 
-      console.log(this.burgers)
+      await this.getStatus();
+    },
+    async getStatus() {
+      const req = await fetch("http://localhost:3000/status");
+
+      const data = await req.json();
+
+      this.status = data;
     }
   },
   mounted() {
